d26bdc489f8ea6ddc6618a1f4da3a96258f3f330
Mohamed Aathif <aathifm99@gmail.com>
2026-05-14 15:56:13 +0530

    fix: update WhatsappIcon svg path to actual brand logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -27,7 +27,8 @@ const InstagramIcon = ({ className }: { className?: string }) => (
 
 const WhatsappIcon = ({ className }: { className?: string }) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
-    <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
+    <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+    <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1" />
   </svg>
 );
 import MagneticElement from './MagneticElement';
